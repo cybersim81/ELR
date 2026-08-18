d28c00c395c017aa761e18bfd4be893f81b69e99
cybersim81 <102663154+cybersim81@users.noreply.github.com>
2026-08-18 12:37:15 +0200

    Update learning_object_repository.py

--- app/persistence/repositories/learning_object_repository.py
+++ app/persistence/repositories/learning_object_repository.py
@@ -18,10 +18,15 @@ class SQLAlchemyLearningObjectRepository(LearningObjectRepository):
         self._session = session
 
     def save(self, learning_object: LearningObject) -> None:
-        model = self._session.get(
-            LearningObjectModel,
-            learning_object.id,
+        model = self._session.identity_map.get(
+            (LearningObjectModel, (learning_object.id,), None)
         )
+
+        if model is None and self._session.bind is not None:
+            model = self._session.get(
+                LearningObjectModel,
+                learning_object.id,
+            )
 
         if model is None:
             model = LearningObjectModel(
@@ -44,6 +49,7 @@ class SQLAlchemyLearningObjectRepository(LearningObjectRepository):
         model.category_id = learning_object.category_id
         model.state = learning_object.state.value
         model.updated_at = learning_object.updated_at
+
 
     def get_by_id(self, object_id: UUID) -> LearningObject | None:
         model = self._session.get(LearningObjectModel, object_id)
