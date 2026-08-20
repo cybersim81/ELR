cf5cfe354e73365fa3ab620c81a937d217931963
cybersim81 <102663154+cybersim81@users.noreply.github.com>
2026-08-20 05:46:38 +0200

    Update __init__.py

--- app/application/services/__init__.py
+++ app/application/services/__init__.py
@@ -1,8 +1,12 @@
+from .category_service import CategoryService
 from .learning_object_service import (
+    LearningObjectNotFound,
     LearningObjectService,
 )
 
 
 __all__ = [
+    "CategoryService",
+    "LearningObjectNotFound",
     "LearningObjectService",
 ]
