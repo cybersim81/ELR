8b12b311d4169253793934fd50f203139b2b9048
cybersim81 <102663154+cybersim81@users.noreply.github.com>
2026-08-21 22:18:41 +0200

    Update test_event_record_repository.py

--- tests/domain/repositories/test_event_record_repository.py
+++ tests/domain/repositories/test_event_record_repository.py
@@ -1,7 +1,5 @@
-from uuid import uuid4
-
 from app.domain.repositories.event_record_repository import EventRecordRepository
 
 
-def test_event_record_repository_is_abstract():
+def test_event_record_repository_defines_required_contract():
     assert EventRecordRepository.__abstractmethods__ == {"save", "get"}
